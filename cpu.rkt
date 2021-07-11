49a109ae2931c8c263f8d6546180e8b034d04971
Edward <edward@correctsoftware.co>
2021-07-11 19:23:15 +0100

    Add initial logic gates

---
+++ cpu.rkt
@@ -0,0 +1,51 @@
+#lang racket
+
+; Int -> Int
+(define (BUF a)
+  (a))
+
+; Int -> Int
+(define (AND a b)
+  (and a b))
+
+; Int -> Int
+(define (OR a b)
+  (or a b))
+
+; Int -> Int
+(define (XOR a b)
+  (cond
+    [(and (= a 0) (= b 1) 1)]
+    [(and (= a 1) (= b 0) 1)]
+    [else                0]))
+
+; Int -> Int
+(define (INV a)
+  (cond
+    [(= a 0) 1]
+    [(= a 1) 0]))
+
+; Int -> Int
+(define (NAND a b)
+  (cond
+    [(and (= a 0) (= b 0) 1)]
+    [(and (= a 0) (= b 1) 1)]
+    [(and (= a 1) (= b 0) 1)]
+    [(and (= a 1) (= b 1) 0)]))
+
+; Int -> Int
+(define (NOR a b)
+  (cond
+    [(and (= a 0) (= b 0) 1)]
+    [(and (= a 0) (= b 1) 0)]
+    [(and (= a 1) (= b 0) 0)]
+    [(and (= a 1) (= b 1) 0)]))
+
+; Int -> Int
+(define (XNOR a b)
+  (cond
+    [(and (= a 0) (= b 0) 1)]
+    [(and (= a 0) (= b 1) 0)]
+    [(and (= a 1) (= b 0) 0)]
+    [(and (= a 1) (= b 1) 1)]))
+    
